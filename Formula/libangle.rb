673f24024c0b4fe4ffad07adad818c7d42003bb5
silibry <silibry@iMac-silibry.local>
2021-11-15 17:58:43 +0300

    add no-undef-prefix

--- Formula/libangle.rb
+++ Formula/libangle.rb
@@ -31,7 +31,7 @@ class Libangle < Formula
           if Hardware::CPU.arm?
             system "gn", "gen", "--args=use_custom_libcxx=false target_cpu=\"arm64\"", "./angle_build"
           else
-            system "gn", "gen", "--args=use_custom_libcxx=false", "./angle_build"
+            system "gn", "gen", "--args=use_custom_libcxx=false,cflags=-Wno-undef-prefix", "./angle_build"
           end
           system "ninja", "-C", "angle_build"
           lib.install "angle_build/libabsl.dylib"
